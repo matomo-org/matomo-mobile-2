3e88eb4444b02ee0d72b181e38dd4f2bccc445fe
Thomas Steur <thomas.steur@mayflower.de>
2012-12-02 16:25:17 +0100

    enable nav buttons on iOS

--- app/controllers/statistics.js
+++ app/controllers/statistics.js
@@ -1,21 +1,20 @@
-/*
- * 
-var leftButtons = [
-    {image:'ic_action_settings.png', width:32},
-    {image:'ic_action_accounts.png', width:32}
-];
-var bar = Ti.UI.createButtonBar({
-    labels: leftButtons,
-    backgroundColor: "#B2AEA5"
-});
+if (OS_IOS) {
+    var leftButtons = [
+        {image:'ic_action_settings.png', width:32},
+        {image:'ic_action_accounts.png', width:32}
+    ];
+    var bar = Ti.UI.createButtonBar({
+        labels: leftButtons,
+        backgroundColor: "#B2AEA5"
+    });
+    
+    bar.addEventListener('click', function () {
+        Alloy.createController('accounts');
+    });
+    
+    $.win1.leftNavButton = bar;
+    
+    $.win1.rightNavButton = Ti.UI.createButton({image:'ic_action_website.png', width:32});
+}
 
-bar.addEventListener('click', function () {
-    Alloy.createController('accounts');
-});
-
-$.win1.leftNavButton = bar;
-
-$.win1.rightNavButton = Ti.UI.createButton({image:'ic_action_website.png', width:32});
-
- */
 $.index.open();
