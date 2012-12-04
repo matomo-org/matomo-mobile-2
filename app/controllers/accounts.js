4c7c62d4600b953ee08bc0fc3d24f370e80cfd08
Florian Eibeck <florian.eibeck@mayflower.de>
2012-12-04 23:10:06 +0100

    Real Accounts are now listed in accounts view.
    Experimented with a back or close button on the accounts view without success. The button is not displayed and the code needs to be fixed.

--- app/controllers/accounts.js
+++ app/controllers/accounts.js
@@ -1,2 +1,35 @@
 
+var accounts =  Alloy.createCollection('AppAccounts');
+accounts.fetch();
+
+var rows = [], len = accounts.length;
+
+accounts.map(function(account) {
+    rows.push(Ti.UI.createTableViewRow({
+        title : account.get("name"),
+        id : account.id
+    }));
+});
+
+$.accounts.setData(rows);
+
+/*
+ * TODO: Button is not displayed in iOS simulator
+ */
+if (OS_IOS) {
+
+    var closeButton = Ti.UI.createButton({
+        title: 'Close',
+        style: Ti.UI.iPhone.SystemButtonStyle.PLAIN
+    });
+
+    closeButton.addEventListener('click', function(){
+        $.index.close();
+    });
+
+    $.index.leftNavButton = closeButton;
+
+}
+
+
 $.index.open();
