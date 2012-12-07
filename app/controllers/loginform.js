81d15be6daa8732061c40f8cb08a2e8f342afa2e
Björn <R>
2012-12-07 15:14:01 +0100

    fixed login behaviour
    * added onBlur for login form url field
    * in case of login error, a message is displayed

--- app/controllers/loginform.js
+++ app/controllers/loginform.js
@@ -4,12 +4,16 @@ var args = arguments[0] || {};
 var accounts = args.accounts || false;
 
 function onUrlReturn () {
+	onUrlBlur();
+	
+	$.username.focus();
+}
+
+function onUrlBlur () {
     if ($.url.value && -1 === $.url.value.indexOf('http')) {
         // user has not specified any http protocol. automatically prepend 'http'.
         $.url.value = 'http://' + $.url.value;
     }
-    
-    $.username.focus();
 }
 
 function onUsernameReturn () {
@@ -29,24 +33,29 @@ function login ()
         console.log(error);
         switch (error) {
             case 'MissingUsername':
+            	alert('Missing Username');
                 break;
             case 'MissingPassword':
+            	allert('Missing Password');
                 break;
             case 'InvalidUrl':
+                alert('Invalid Url');
                 break;
             case 'ReceiveAuthTokenError':
+            	alert('Recieve Auth Token Error');
                 break;
             case 'NoViewAccess':
-                break;
-            case 'ReceiveAuthTokenError':
+            	alert('No View Access');
                 break;
             default:
+            	allert('An unknown error has occured:\n' + error)
                 return;
         }
         // output message
     });
     
     if (!account.isValid()) {
+        alert('URL must not be empty!\nfoo\nbar');
         return;
     }
     
@@ -61,7 +70,7 @@ function login ()
         
         dialog.show();
     });
-    
+
     account.on('change:tokenAuth', function (accountModel, tokenAuth) {
         console.log('change:tokenAuth');
         var site = Alloy.createModel('piwikEntrySite');
@@ -81,6 +90,6 @@ function login ()
         });
         
     });
-    
+
     account.requestAuthToken();
 }
