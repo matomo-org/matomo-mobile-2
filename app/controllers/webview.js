c23b504b9857511ca628132cb671024299d3c4c7
Thomas Steur <thomas.steur@mayflower.de>
2012-12-09 10:56:58 +0100

    added close button

--- app/controllers/webview.js
+++ app/controllers/webview.js
@@ -2,6 +2,20 @@ var args     = arguments[0] || {};
 var webUrl   = args.url || false;
 var webTitle = args.title || false;
 
+if (OS_IOS) {
+
+    var closeButton = Ti.UI.createButton({
+        title: 'Close',
+        style: Ti.UI.iPhone.SystemButtonStyle.PLAIN
+    });
+
+    closeButton.addEventListener('click', function(){
+        $.index.close();
+    });
+
+    $.web.leftNavButton = closeButton;
+}
+
 exports.open = function () {
     $.web.title   = webTitle;
     $.webview.url = webUrl;
