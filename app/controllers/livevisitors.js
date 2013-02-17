76042fa99ab5fc94d4a13f07aef8406219bd3572
Thomas Steur <thomas.steur@mayflower.de>
2013-02-17 15:53:33 +0100

    partial auto reload implementation

--- app/controllers/livevisitors.js
+++ app/controllers/livevisitors.js
@@ -17,6 +17,30 @@ if (OS_IOS) {
     $.pullToRefresh.init($.liveTable);
 }
 
+var refreshTimer = null;
+var stopRefreshTimer = function () {
+
+    if (refreshTimer) {
+        // do no longer execute autoRefresh if user opens another app or returns the home screen (only for iOS)
+        clearTimeout(refreshTimer);
+    }
+    
+    $.countdown.stop();
+};
+
+var startRefreshTimer = function (timeoutInMs) {
+    stopRefreshTimer();
+
+    $.countdown.init(parseInt(timeoutInMs / 1000));
+    $.countdown.start();
+
+    refreshTimer = setTimeout(function () {
+        if (doRefresh) {
+            doRefresh();
+        }
+    }, timeoutInMs);
+}
+
 function doOpenVisitor(event)
 {
     if (!event || !event.rowData || !event.rowData.visitor) {
@@ -30,7 +54,6 @@ function doOpenVisitor(event)
 
 function render(account, counter30Min, counter24Hours, visitorDetails)
 {
-    $.countdown.start();
     showReportContent();
 
     var rows = [];
@@ -54,6 +77,8 @@ function render(account, counter30Min, counter24Hours, visitorDetails)
 
     $.liveTable.setData(rows);
     rows = null;
+
+    startRefreshTimer(refreshIntervalInMs);
 }
 
 function doChooseReport()
@@ -77,7 +102,7 @@ function showLoadingMessage()
     } 
 
     $.loadingindicator.show();
-    $.countdown.stop();
+    stopRefreshTimer();
 }
 
 function onFetchError()
@@ -90,5 +115,64 @@ function doRefresh()
     showLoadingMessage();
     piwikLiveVisitors.fetchVisitors(accountModel, siteModel.id, render, onFetchError);
 }
+
+/***** HANDLE BACKGROUND EVENTS ******/
+    /*
+    this.addEventListener('closeWindow', function () {
+        if (stopRefreshTimer) {
+            stopRefreshTimer();
+        }
+    });
+
+    this.addEventListener('blurWindow', function () {
+        if (stopRefreshTimer) {
+            stopRefreshTimer();
+        }
+    });
+
+    this.addEventListener('focusWindow', function () {
+        if (params && params.autoRefresh && tableView && tableView.data && tableView.data.length && that) {
+            // start auto refresh again if user returns to this window from a previous displayed window
+            
+            startRefreshTimer(20000);
+        }
+    });
+    */
+    
+    var onResume = function () {
+        if ($.liveTable && $.liveTable.data && $.liveTable.data.length) {
+
+            startRefreshTimer(3000);
+        }
+    };
+    
+    var onPause = function () {
+        if (stopRefreshTimer) {
+            stopRefreshTimer();
+        }
+    }
+    
+    // ios
+    Ti.App.addEventListener('resume', onResume);
+    Ti.App.addEventListener('pause', onPause);
+
+    var activity = null;
+    
+    // get activity on android
+    if (Ti.Android && Ti.Android.currentActivity) {
+        activity = Ti.Android.currentActivity;
+    } else if (!activity && Ti.UI.currentWindow && Ti.UI.currentWindow.activity) {
+        activity = Ti.UI.currentWindow.activity;
+    } else if (!activity && $.index && $.index.activity) {
+        activity = $.indexactivity;
+    }
+
+    // android
+    if (activity) {
+        activity.addEventListener('pause', stopRefreshTimer);
+        activity.addEventListener('stop', stopRefreshTimer);
+    }
+
+/***** HANDLE BACKGROUND EVENTS END ******/
 
 exports.refresh = doRefresh;
