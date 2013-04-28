6df2b64ccd55a8dcdd245f200deb6d1fc6584f78
Thomas Steur <thomas.steur@mayflower.de>
2013-04-28 11:47:30 +0200

    display report logo in reports if one is present

--- app/controllers/report_row.js
+++ app/controllers/report_row.js
@@ -1,6 +1,21 @@
 var processedReport = arguments[0] || {};
 
+function showLogo(processedReport)
+{
+    var accountModel = require('session').getAccount();
+    
+    $.icon.width  = processedReport.getLogoWidth() || 16;
+    $.icon.height = processedReport.getLogoHeight() || 16;
+    $.icon.left   = OS_ANDROID ? '10dp' : 10;
+    $.icon.image  = accountModel.getBasePath() + processedReport.getLogo();
+    $.icon.show();
+}
+
 if (processedReport) {
     $.title.text = processedReport.getTitle();
     $.value.text = processedReport.getValue();
+
+    if (processedReport.hasLogo()) {
+        showLogo(processedReport);
+    }
 } 
