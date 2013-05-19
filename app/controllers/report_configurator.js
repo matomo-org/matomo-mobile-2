3da12fdf14e0a90863712231b63a3506f66c2944
Thomas Steur <thomas.steur@mayflower.de>
2013-05-19 10:46:52 +0200

    display some websites within the right menu

--- app/controllers/report_configurator.js
+++ app/controllers/report_configurator.js
@@ -10,12 +10,59 @@ function hideRightSidebar () {
 function open()
 {
     require('layout').setRightSidebar($.index);
+    fetchWebsites();
 }
 
 function toggleVisibility() 
 {
     require('layout').toggleRightSidebar();
 }
+
+function selectWebsite(event)
+{
+    if (!event || !event.source || null === event.source.modelid) {
+        console.log('ModelID in source not defined, cannot select website');
+        return;
+    }
+
+    var id      = event.source.modelid;
+    var website = $.piwikProcessedReport.get(id);
+
+    if (!website) {
+        console.log('websiteModel not found in collection, cannot select website');
+        return;
+    }
+
+    var siteModel = Alloy.createModel('PiwikWebsites', {idsite: website.get('reportMetadata').idsite,
+                                                        name: website.get('label')});
+    var account   = require('session').getAccount();
+
+    require('session').setWebsite(siteModel, account);
+    toggleVisibility();
+}
+
+function fetchWebsites()
+{
+    var reportDate = require('session').getReportDate();
+    var website    = require('session').getWebsite();
+    var account    = require('session').getAccount();
+
+    $.piwikProcessedReport.fetchProcessedReports('nb_visits', {
+        params: {
+            period: reportDate.getPeriodQueryString(), 
+            date: reportDate.getDateQueryString(), 
+            idSite: website.get('idsite'),
+            filter_limit: 25,
+            hideMetricsDoc: 1,
+            apiModule: "MultiSites",
+            apiAction: "getAll",
+            showColumns: 'nb_visits'
+        },
+        account: account
+    });
+}
+
+require('session').on('accountChanged', fetchWebsites);
 
 exports.open = open;
 exports.toggleVisibility = toggleVisibility;
