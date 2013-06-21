e6dbd4972ee7005ff86630f292263ec5b41985d2
Thomas Steur <thomas.steur@googlemail.com>
2013-06-21 15:23:07 +0200

    test number of entries after putting a value into cache and refactored tests

--- app/assets/specs/models/persistentCache_spec.js
+++ app/assets/specs/models/persistentCache_spec.js
@@ -7,14 +7,33 @@
 
 require('behave').andSetup(this);
 
+function getPersistentCache()
+{
+    var Alloy = require('alloy');
+    var cache = Alloy.createCollection('persistentCache');
+
+    return cache;
+}
+
+function expectNumberOfEntriesToBeInCache(cache, numEntries)
+{
+    expect(cache.length).toBe(numEntries);
+}
+
 describe('persistentCache models', function() {
 
-    it('should have no cache entries by default', function(){
-        var Alloy = require('alloy');
-        var cache = Alloy.createCollection('persistentCache');
+    it('should have no cache entries by default', function() {
+        var cache = getPersistentCache();
         cache.fetch();
 
-        expect(cache.length).toBe(0);
+        expectNumberOfEntriesToBeInCache(cache, 0);
     });
 
+    it('should have one cache entry after putting a value', function() {
+        var cache = getPersistentCache();
+
+        cache.put('testkey', 'myvalue', 10);
+
+        expectNumberOfEntriesToBeInCache(cache, 1);
+    });
 });
