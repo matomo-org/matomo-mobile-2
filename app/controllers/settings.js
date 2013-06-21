a9659f1cc313d351188f055bfc58092732f28b8f
Thomas Steur <thomas.steur@googlemail.com>
2013-06-21 22:01:49 +0200

    fixes #3983 footer title is displayed weird on iOS. Looks like Alloy does not support ListView.footerView yet but works creating the widgets the old way

--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -15,6 +15,14 @@ var callbacks = {changeLanguage: changeLanguage,
                  toggleGraphsEnabled: toggleGraphsEnabled,
                  changeHttpTimeout: changeHttpTimeout};
 
+if (OS_IOS) {
+    var margin     = Alloy.isTablet ? 30 : 15
+    var footerView = Ti.UI.createView({left: margin, right: margin, height: Ti.UI.SIZE});
+    footerView.add(Ti.UI.createLabel({text: L('Mobile_LoginToPiwikToChangeSettings')}));
+    $.settingsTable.footerView = footerView;
+    footerView = null;
+}
+
 function onOpen()
 {
     require('Piwik/Tracker').trackWindow('Settings', 'settings');
