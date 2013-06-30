f82da496f4960606f92b4d80849542c290a5dc67
Thomas Steur <thomas.steur@googlemail.com>
2013-06-30 22:57:22 +0200

    change log level from trace to debug

--- tools/deploy/ipad_simulator.sh
+++ tools/deploy/ipad_simulator.sh
@@ -1,3 +1,3 @@
 export PATH=/opt/local/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/MacGPG2/bin
 
-titanium build -p ios -F ipad -T simulator --log-level trace --ios-version 6.1
+titanium build -p ios -F ipad -T simulator --log-level debug --ios-version 6.1
