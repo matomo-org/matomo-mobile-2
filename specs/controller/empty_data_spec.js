2a3d2feb68a9d92cb0cd7aaefa23b70472f3209a
Thomas Steur <thomas.steur@googlemail.com>
2013-06-30 22:03:46 +0200

    added another test to make sure default values are correct

--- specs/controller/empty_data_spec.js
+++ specs/controller/empty_data_spec.js
@@ -7,13 +7,36 @@
 
 var Alloy = require('alloy');
 
+function createController(options)
+{
+    return Alloy.createController('empty_data', options);
+}
+
+function expectedMessageToBeEqual(controller, message)
+{
+    expect(controller.message.text).toEqual(message);
+}
+
+function expectedTitleToBeEqual(controller, message)
+{
+    expect(controller.title.text).toEqual(message);
+}
+
 describe('empty data controller', function() {
+
+    it('should use a default title but no default message', function() {
+
+        var emptyData = createController();
+
+        expectedTitleToBeEqual(emptyData, require('L')('Mobile_NoDataShort'));
+        expectedMessageToBeEqual(emptyData, null);
+    });
 
     it('should display the passed message and title', function() {
 
-        var emptyData = Alloy.createController('empty_data', {message: 'Test message', title: 'Test Title'});
+        var emptyData = createController({message: 'Test message', title: 'Test Title'});
 
-        expect(emptyData.message.text).toEqual('Test message');
-        expect(emptyData.title.text).toEqual('Test Title');
+        expectedTitleToBeEqual(emptyData, 'Test Title');
+        expectedMessageToBeEqual(emptyData, 'Test message');
     });
 });
