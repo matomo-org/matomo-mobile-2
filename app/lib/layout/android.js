e261c3cc7527cb443483c3f6ab795c1a2d8389c9
Thomas Steur <thomas.steur@googlemail.com>
2013-07-03 20:32:18 +0200

    this is a workaround for user needs to press the back button twice to close the app on Android. We should look for a better solution later. Also fixes the splashscreen is visible when switching between reports

--- app/lib/layout/android.js
+++ app/lib/layout/android.js
@@ -9,6 +9,26 @@ var Alloy = require('alloy');
 var Backbone = Alloy.Backbone;
 var _ = Alloy._;
 
+var numCurrentlyOpenedWindows = 0;
+
+// overlay splashscreen
+var win = Ti.UI.createWindow({backgroundColor: "#e5e5e5"});
+win.open();
+
+function forceCloseApp()
+{
+    Ti.Android.currentActivity && Ti.Android.currentActivity.finish();
+}
+
+function quitAppIfNoNewWindowOpened()
+{
+    if (0 >= numCurrentlyOpenedWindows) {
+        forceCloseApp();
+    }
+}
+
+var checkAppShouldBeClosedTimeout = null;
+
 function AndroidLayout()
 {
     function close (win) {
@@ -18,6 +38,16 @@ function AndroidLayout()
 
         win.close();
 
+        if (checkAppShouldBeClosedTimeout) {
+            clearTimeout(checkAppShouldBeClosedTimeout);
+        }
+
+        numCurrentlyOpenedWindows--;
+
+        if (0 >= numCurrentlyOpenedWindows) {
+            checkAppShouldBeClosedTimeout = setTimeout(quitAppIfNoNewWindowOpened, 700);
+        }
+
         win = null;
     }
 
@@ -25,6 +55,12 @@ function AndroidLayout()
         if (!win) {
             return;
         }
+
+        if (checkAppShouldBeClosedTimeout) {
+            clearTimeout(checkAppShouldBeClosedTimeout);
+        }
+
+        numCurrentlyOpenedWindows++;
         
         win.addEventListener('androidback', closeThisWindow);
         this.trigger('open', win);
