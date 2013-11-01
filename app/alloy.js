a05e9a467be72480f6e95faf89925b8f7198336a
Thomas Steur <thomas.steur@gmail.com>
2013-11-01 20:55:57 +1300

    fix detection of iOS7

--- app/alloy.js
+++ app/alloy.js
@@ -7,7 +7,7 @@
 
 Alloy.isTablet = require('Piwik/Platform').isTablet;
 Alloy.isHandheld = !Alloy.isTablet;
-Alloy.isIOS7OrLater = OS_IOS && (7 <= Ti.Platform.version);
+Alloy.isIOS7OrLater = OS_IOS && (7 <= parseInt(Ti.Platform.version, 10));
 
 if (OS_IOS) {
     Alloy.statusBarStyle = Ti.UI.iPhone.StatusBar.LIGHT_CONTENT;
