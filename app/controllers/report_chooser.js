2b659ebfe2ee9ab19699e3a475486d597360534e
Thomas Steur <thomas.steur@gmail.com>
2014-10-09 17:42:11 +1300

    refs #5306 fixes scroll position on iPhone 4s after opening the left sidebar. Still needs to be tested for any side effects on other iOS and Android devices

--- app/controllers/report_chooser.js
+++ app/controllers/report_chooser.js
@@ -67,6 +67,11 @@ function updateAvailableReportsList()
 
     makeSureSelectedRowIsStillSelected();
 
+    if ($.reportsTable.scrollToTop) {
+        // fix "iPhone 4s, iOS 8: Left sidebar is positioned too far down after opening it" see https://github.com/piwik/piwik-mobile-2/issues/5306
+        $.reportsTable.scrollToTop(0);
+    }
+    
     rows = null;
 }
 
