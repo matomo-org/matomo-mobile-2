5c98bba8bef18ef8d0e7c9ea622cf59469280a6f
Thomas Steur <tsteur@users.noreply.github.com>
2017-05-17 15:58:06 +1200

    fix #5369 make sure Piwik formats all metrics

--- app/models/piwikProcessedReport.js
+++ app/models/piwikProcessedReport.js
@@ -32,7 +32,8 @@ exports.definition = {
         "defaultParams": {
             hideMetricsDoc: 1, 
             showTimer: 0,
-            filter_limit: 40
+            filter_limit: 40,
+            format_metrics: '1'
         }
     },        
 
