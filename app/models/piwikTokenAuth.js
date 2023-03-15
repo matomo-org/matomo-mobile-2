ee1178f323ca1f65cc73e2d982aad6c960347098
Thomas Steur <tsteur@users.noreply.github.com>
2023-03-15 20:05:41 +1300

    fix #5444 Update user agent to fix issue with some Matomo installs

--- app/models/piwikTokenAuth.js
+++ app/models/piwikTokenAuth.js
@@ -53,10 +53,14 @@ exports.definition = {
                     }
 
                     var passwordHash = this.getPasswordHash(password);
-
-                    var params = {userLogin: username, md5Password: passwordHash, method: 'UsersManager.getTokenAuth'};
+                    var params = {
+                        'userLogin': username,
+                        'passwordConfirmation': password,
+                        'description': 'Matomo Mobile 2',
+                        'method': 'UsersManager.createAppSpecificTokenAuth'
+                    };
                     if (authCode) {
-                    	    params.authCode = authCode;
+                        params.authCode = authCode;
                     }
 
                     //first test using matomo 3.X
@@ -66,14 +70,10 @@ exports.definition = {
                         params: params,
                         success: onSuccess, 
                         error: function (undefined, error) {
-                            if (error && error.getMessage() && error.getMessage().indexOf('getTokenAuth') > 0) {
-                                // now test for matomo 4.x
-                                var params = {
-                                    'userLogin': username,
-                                    'passwordConfirmation': password,
-                                    'description': 'Matomo Mobile 2',
-                                    'method': 'UsersManager.createAppSpecificTokenAuth'
-                                };
+                            if (error && error.getMessage() && error.getMessage().indexOf('createAppSpecificTokenAuth') > 0) {
+                                // now test for matomo 3.x
+
+                                var params = {userLogin: username, md5Password: passwordHash, method: 'UsersManager.getTokenAuth'};
                                 if (authCode) {
                                     params.authCode = authCode;
                                 }
